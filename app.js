6d47556795645be2f1a291777f866d4c0d57eee3
Chakit <chakitbhandari93@gmail.com>
2022-12-09 11:23:29 +0530

    final commit

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const express = require("express");
 const app = express();
 const book = require("./book.js");
-console.log(book);
+// console.log("book : ",book);
+// console.log("module in app.js file : ",module);
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use("/api/books",book);
+app.get("/api/health",validate1,validate2,(req,res,next) => {
+    console.log("Inside health /");
+    res.send();
+});
+
+function validate1(req,res,next){
+    console.log("Inside validate1 middleware");
+    next();
+}
+
+function validate2(req,res,next){
+    console.log("Inside validate2 middleware");
+    next();
+}
 
 app.listen(3000,() => {
     console.log("Server listening on port 3000");
--- book.js
+++ book.js
@@ -1,8 +1,53 @@
 const {Router} = require("express");
 const route = Router();
 
-console.log("module : ",module);
+const books = {
+    1:"Hindi",
+    2:"English"
+};
+
+const courses = {
+    1:"ECE",
+    2:"CSE"
+};
+// console.log("Inside book.js file");
 
 route.get("/",function(req,res){
-    console.log("Inside /");
+    console.log("Inside books /");
 });
+
+route.get("/:bookid",(req,res) => {
+    try{
+        if(!books.hasOwnProperty(req.params.bookid))
+            throw "This book doesn't exist";
+        res.send(books[req.params.bookid]);
+    }catch (err){
+        console.log(err);
+        res.send(err);
+    }
+});
+
+route.get("/courses/:courseid",(req,res) => {
+    try{
+        if(!courses.hasOwnProperty(req.params.courseid))
+            throw "This course doesn't exist";
+        res.send(courses[req.params.courseid]);
+    }catch (err){
+        console.log(err);
+        res.send(err);
+    }
+});
+
+
+route.post("/addbook",(req,res) => {
+    books[req.body.id] = req.body.subject;
+    res.send("book added!!");
+});
+
+route.post("/addcourse",(req,res) => {
+    courses[req.body.id] = req.body.course;
+    res.send();
+});
+
+module.exports = route;
+// console.log("module in book.js file : ",module.id);
